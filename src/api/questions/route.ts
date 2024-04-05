a95c3a284b25b59b1c0e713f8115987d5baac82f
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-04-05 15:49:44 +0100

    change url api

--- src/api/questions/route.ts
+++ src/api/questions/route.ts
@@ -8,11 +8,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const nextId = idAsNumber + 1
     return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
-    <meta property="og:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/images/ninja-${id}.jpeg" />
+    <meta property="og:image" content="https://farcaster-frames-sigma.vercel.app/images/ninja-${id}.jpeg" />
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/ninja-${id}.jpeg" />
+    <meta property="fc:frame:image" content="https://farcaster-frames-sigma.vercel.app/ninja-${id}.jpeg" />
     <meta property="fc:frame:button:1" content="Next Question" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=${nextId}" />
+    <meta property="fc:frame:post_url" content="https://farcaster-frames-sigma.vercel.app/api/basic?id=${nextId}" />
   </head></html>`)
 }
 
