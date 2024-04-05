6f164e098c30337714f05671704a4f66de68fbde
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-04-05 16:33:46 +0100

    Fix the url

---
+++ pages/api/question.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const frame = await getResponse(req);
+    res.status(200).send(frame);
+}
+
+async function getResponse(req: NextApiRequest): Promise<string> {
+    const searchParams = req.query;
+    const id = Array.isArray(searchParams.id) ? searchParams.id[0] : searchParams.id;
+    const idAsNumber = id ? Number(id) : 1;
+    const nextId = idAsNumber + 1;
+
+    const htmlContent = `<!DOCTYPE html>
+        <html>
+        <head>
+            <title>This is frame ${id}</title>
+            <meta property="og:image" content="https://farcaster-frames-sigma.vercel.app/images/ninja-${id}.jpeg" />
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="https://farcaster-frames-sigma.vercel.app/images/ninja-${id}.jpeg" />
+            <meta property="fc:frame:button:1" content="Next Question" />
+            <meta property="fc:frame:post_url" content="https://farcaster-frames-sigma.vercel.app/api/question?id=${nextId}" />
+        </head>
+        </html>`;
+
+    return htmlContent;
+}
