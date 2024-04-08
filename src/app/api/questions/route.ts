fe88829150fbc051a97cc305e780b20953ec944f
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-04-08 17:11:06 +0100

    Add question options

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,26 +1,29 @@
-import { config } from '@/config/config'
-import { NextRequest, NextResponse } from 'next/server'
+import { config } from '@/config/config';
+import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams
-  const id = searchParams.get('id')
+  const searchParams = req.nextUrl.searchParams;
+  const id = searchParams.get('id');
+  const response = searchParams.get('re');
 
-  const idAsNumber = id ? Number(id) : 1
+  console.log({ id, response })
+
+  const idAsNumber = id && id.trim() !== '' ? Number(id) : 1;
 
   const nextId = idAsNumber + 1
 
-  if (idAsNumber === 4) {
+  if (idAsNumber === 20) {
     return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 4</title>
-    <meta property="og:image" content="${config.hostUrl}/images/ninja-4.jpeg" />
+    <title>This is Last Question</title>
+    <meta property="og:image" content="${config.hostUrl}/images/evveland_completed.jpeg" />
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${config.hostUrl}/images/ninja-4.jpeg" />
+    <meta property="fc:frame:image" content="${config.hostUrl}/images/evveland_completed.jpeg" />
     <meta property="fc:frame:button:1" content="Learn More!" />
     <meta property="fc:frame:button:1:action" content="link" />
     <meta property="fc:frame:button:1:target" content="https://evveland.com" />
-    <meta property="fc:frame:button:2" content="Restart Quiz!" />
+    <meta property="fc:frame:button:2" content="Mint NFT!" />
     <meta property="fc:frame:button:2:action" content="post" />
-    <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/questions?id=1" />
+    <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/mint" />
     </head></html>`)
   }
 
@@ -29,13 +32,23 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="og:image" content="${config.hostUrl}/images/ninja-${id}.jpeg" />
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${config.hostUrl}/images/ninja-${id}.jpeg" />
-    <meta property="fc:frame:button:1" content="Next Question" />
-    <meta property="fc:frame:post_url" content="${config.hostUrl}/api/questions?id=${nextId}" />
-  </head></html>`)
+    <meta property="fc:frame:button:1" content="A" />
+    <meta property="fc:frame:button:2" content="B" />
+    <meta property="fc:frame:button:3" content="C" />
+    <meta property="fc:frame:button:4" content="D" />
+    <meta property="fc:frame:button:1:action" content="post" />
+    <meta property="fc:frame:button:1:target" content="${config.hostUrl}/api/questions?id=${nextId}&re=A" />
+    <meta property="fc:frame:button:2:action" content="post" />
+    <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/questions?id=${nextId}&re=B" />
+    <meta property="fc:frame:button:3:action" content="post" />
+    <meta property="fc:frame:button:3:target" content="${config.hostUrl}/api/questions?id=${nextId}&re=C" />
+    <meta property="fc:frame:button:4:action" content="post" />
+    <meta property="fc:frame:button:4:target" content="${config.hostUrl}/api/questions?id=${nextId}&re=D" />
+  </head></html>`);
 }
 
-export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req)
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  return getResponse(req);
 }
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
