cf96e393a65ed1812aff34608ed01badead92cb9
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-04-15 15:35:51 +0100

    Add leaderboard

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -52,7 +52,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const completionTimeMs = new Date(stopTime).getTime() - new Date(userData.startTime).getTime();
     userData.completionTime = completionTimeMs.toString();
     const htmlContent = `<!DOCTYPE html><html><head>
-      <title>This is Last Question</title>
+      <title>Join Leaderboard</title>
       <meta property="og:image" content="${config.hostUrl}/images/result.png" />
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${config.hostUrl}/images/result.png" />
@@ -61,34 +61,36 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       <meta property="fc:frame:button:1:target" content="https://evveland.com" />
       <meta property="fc:frame:button:2" content="Join Leaderboard!" />
       <meta property="fc:frame:button:2:action" content="post" />
-      <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/leaderboard?address=${userAddress}&data=${userData} />
+      <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/leaderboard?address=${userAddress}&data=${userData}" />
       </head></html>`
 
     return new NextResponse(htmlContent)
 
+  } else {
+    const htmlContent = `<!DOCTYPE html><html><head>
+    <title>This is frame ${nextId}</title>
+    <meta property="og:image" content="${config.hostUrl}/images/Question-${nextId}.png" />
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:user:timestamp" content= ""/>
+    <meta property="fc:frame:image" content="${config.hostUrl}/images/Question-${nextId}.png" />
+    <meta property="fc:frame:button:1" content="A" />
+    <meta property="fc:frame:button:2" content="B" />
+    <meta property="fc:frame:button:3" content="C" />
+    <meta property="fc:frame:button:4" content="D" />
+    <meta property="fc:frame:button:1:action" content="post" />
+    <meta property="fc:frame:button:1:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=A" />
+    <meta property="fc:frame:button:2:action" content="post" />
+    <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=B" />
+    <meta property="fc:frame:button:3:action" content="post" />
+    <meta property="fc:frame:button:3:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=C" />
+    <meta property="fc:frame:button:4:action" content="post" />
+    <meta property="fc:frame:button:4:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=D" />
+    </head></html>`;
+
+    return new NextResponse(htmlContent)
   }
 
-  const htmlContent = `<!DOCTYPE html><html><head>
-      <title>This is frame ${nextId}</title>
-      <meta property="og:image" content="${config.hostUrl}/images/Question-${nextId}.png" />
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:user:timestamp" content= ""/>
-      <meta property="fc:frame:image" content="${config.hostUrl}/images/Question-${nextId}.png" />
-      <meta property="fc:frame:button:1" content="A" />
-      <meta property="fc:frame:button:2" content="B" />
-      <meta property="fc:frame:button:3" content="C" />
-      <meta property="fc:frame:button:4" content="D" />
-      <meta property="fc:frame:button:1:action" content="post" />
-      <meta property="fc:frame:button:1:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=A" />
-      <meta property="fc:frame:button:2:action" content="post" />
-      <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=B" />
-      <meta property="fc:frame:button:3:action" content="post" />
-      <meta property="fc:frame:button:3:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=C" />
-      <meta property="fc:frame:button:4:action" content="post" />
-      <meta property="fc:frame:button:4:target" content="${config.hostUrl}/api/answers?id=${nextId}&re=D" />
-      </head></html>`;
 
-  return new NextResponse(htmlContent)
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
