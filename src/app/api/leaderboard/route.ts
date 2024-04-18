3b7ddb0601319219010db896eec785ed62c6fd61
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-04-18 10:57:23 +0100

    Write file to disk

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -3,17 +3,22 @@ import satori from 'satori';
 import sharp from 'sharp';
 import { getAllUserData } from '@/utils/connectToDatabase';
 import { UserData } from '../questions/route';
-import { join } from 'path';
 import * as fs from 'fs';
 import { generateJSX } from '../common';
 import { config } from '@/config/config';
 import { computeHtml } from '@/utils/compute-html';
+import { promises as fsPromises } from 'fs';
+import { join } from 'path';
 
 const fontPath = join(process.cwd(), 'Roboto-Regular.ttf');
 const fontData = fs.readFileSync(fontPath);
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
     try {
+         // Ensure that the images directory exists
+         const imagesDir = join(process.cwd(), 'images');
+         await fsPromises.mkdir(imagesDir, { recursive: true });
+
         const userDataList: UserData[] = (await getAllUserData()).slice(0, 10);
         const jsx = generateJSX(userDataList);
 
@@ -31,11 +36,13 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
         const pngBuffer = await sharp(Buffer.from(svg)).toFormat('png').toBuffer();
    
-        fs.writeFileSync('./images/leaderboard.png', pngBuffer);
+         // Write the image to the images directory
+        const imagePath = join(imagesDir, 'leaderboard.png');
+        await fsPromises.writeFile(imagePath, pngBuffer);
 
        console.log("File successfully written to disk")
         return new NextResponse(computeHtml({
-            imagePath: "/images/leaderboard.png",
+            imagePath: imagePath,
             postType: "leaderboard",
             content: "Leaderboard"
         }))
