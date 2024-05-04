d4874c3a92751844cf2d4ebdfeb43403685d2a71
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-05-04 18:56:27 +0100

    Refactor data source

--- src/config/data.ts
+++ src/config/data.ts
@@ -165,4 +165,30 @@ export const Leaders: WarpHeroesLeaders[] = [
         profileURL:
             "https://e7.pngegg.com/pngimages/527/535/png-clipart-gamecube-controller-nintendo-game-controllers-video-game-consoles-cube-purple-blue.png",
     },
+
+    {
+        name: "Nexus",
+        description:
+            " Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final.",
+        username: "evveland.xyz",
+        profileURL:
+            "https://e7.pngegg.com/pngimages/527/535/png-clipart-gamecube-controller-nintendo-game-controllers-video-game-consoles-cube-purple-blue.png",
+    },
+    {
+        name: "Nexus",
+        description:
+            " Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final.",
+        username: "evveland.xyz",
+        profileURL:
+            "https://e7.pngegg.com/pngimages/527/535/png-clipart-gamecube-controller-nintendo-game-controllers-video-game-consoles-cube-purple-blue.png",
+    },
+    {
+        name: "Nexus",
+        description:
+            " Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final.",
+        username: "evveland.xyz",
+        profileURL:
+            "https://e7.pngegg.com/pngimages/527/535/png-clipart-gamecube-controller-nintendo-game-controllers-video-game-consoles-cube-purple-blue.png",
+    },
+
 ];
