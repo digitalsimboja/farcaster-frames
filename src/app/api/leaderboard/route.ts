2b0fce2dc8540ed773f4e73d561147877f838a86
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-06-10 16:42:59 +0100

    Fix mint

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -45,7 +45,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
        <meta property="fc:frame:button:1:target" content="https://evveland.com" />
        <meta property="fc:frame:button:2" content="Mint Free WarpHero Pass!" /> 
        <meta property="fc:frame:button:2:action" content="post" />
-       <meta property="fc:frame:button:2:target" content="${config.hostUrl}/api/mint?type=mint" />
+       <meta property="fc:frame:button:2:target"  content="https://warpcast.com/digitalsimboja/0xc7903af7" />
        </head></html>`
 
         return new NextResponse(htmlContent)
